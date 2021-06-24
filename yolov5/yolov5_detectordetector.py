329e94ae740d568e0e2fe14173e15b62e271c403
ff_wang <1105936347@qq.com>
2021-06-24 21:22:38 +0800

    :alien: 可以忽略的修改

--- yolov5/yolov5_detectordetector.py
+++ yolov5/yolov5_detectordetector.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
  Created on 2021/6/22 8:57
- Filename   : detector.py
+ Filename   : yolov5_detector.py
  Author     : Taosy.W
  Zhihu      : https://www.zhihu.com/people/1105936347
  Github     : https://github.com/AFei19911012
