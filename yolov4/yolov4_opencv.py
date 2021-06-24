9dd91fa52c46f9ca25997f817bd07da28af7bf0f
ff_wang <1105936347@qq.com>
2021-06-24 11:47:30 +0800

    :art: 重构 yolov4-opencv 检测器代码

--- yolov4/yolov4_opencv.py
+++ yolov4/yolov4_opencv.py
@@ -8,135 +8,183 @@
  Description: YOLO-OpenCV
 """
 
-# Source: https://github.com/spmallick/learnopencv/tree/master/ObjectDetection-YOLO
-
-import cv2 as cv
-import argparse
-import sys
+import cv2
 import numpy as np
-import os.path
 import random
 
 
-def get_classes(classes_path):
-    with open(classes_path, 'rt') as fid:
-        classes = fid.read().rstrip('\n').split('\n')
-    return classes
+class YOLOv4DetectorCV:
+    """ YOLOv4-OpenCV 图像检测 """
+
+    def __init__(self, weights='weights/yolov4.weights', class_file='config/coco.names', cfg='config/coco.cfg', conf_thres=0.25, nms_thres=0.45, imgsz=512,
+                 device='gpu'):
+        """ Initialization """
+        self.weights = weights
+        self.class_file = class_file
+        self.cfg = cfg
+        self.conf_thres = conf_thres
+        self.nms_thres = nms_thres
+        """ 320 速度更快 """
+        self.imgsz = imgsz
+        """ gpu 或者 cpu """
+        self.device = device
+        """ net """
+        self.net = self.init_net()
+        """ names """
+        self.classes = self.get_names()
+        """ colors """
+        self.colors = self.get_colors()
+
+    def init_net(self):
+        """ 初始化网络 """
+        """ Give the configuration and weight files for the model and load the network using them """
+        net = cv2.dnn.readNetFromDarknet(self.cfg, self.weights)
+        """ 设置 gpu 或者 cpu """
+        if self.device == 'cpu':
+            net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+            net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+            print('Using CPU device.')
+        else:
+            net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+            print('Using GPU device.')
+        return net
+
+    def get_names(self):
+        """ 读取类别文件 """
+        with open(self.class_file, 'rt') as f:
+            classes = f.read().rstrip('\n').split('\n')
+            return classes
+
+    def get_colors(self):
+        """ 生成颜色 """
+        return {name: (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for name in self.classes}
+
+    def get_output_names(self):
+        """ Get the names of the output layers """
+        """ Get the names of all the layers in the network """
+        layersNames = self.net.getLayerNames()
+        """ Get the names of the output layers, i.e. the layers with unconnected outputs """
+        return [layersNames[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+    def draw_image(self, frame, classId, conf, left, top, right, bottom):
+        """ Draw the predicted bounding box """
+        """ line thickness """
+        tl = 2 or round(0.002 * (frame.shape[0] + frame.shape[1]) / 2) + 1
+        c1 = (left, top)
+        c2 = (right, bottom)
+        color = self.colors[self.classes[classId]]
+        """ Draw a bounding box """
+        cv2.rectangle(frame, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+        """ Get the label for the class name and its confidence """
+        label_show = f'{self.classes[classId]}_{conf:.2f}'
+        """ font thickness """
+        tf = max(tl - 1, 1)
+        """ Display the label at the top of the bounding box """
+        t_size = cv2.getTextSize(label_show, cv2.FONT_HERSHEY_SIMPLEX, fontScale=tl/3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        """ filled """
+        cv2.rectangle(frame, c1, c2, color, cv2.FILLED, cv2.LINE_AA)
+        cv2.putText(frame, label_show, (c1[0], c1[1] - 2), cv2.FONT_HERSHEY_SIMPLEX, tl/3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+    def non_max_suppression(self, frame, outs):
+        """ Remove the bounding boxes with low confidence using non-maxima suppression """
+        frameHeight = frame.shape[0]
+        frameWidth = frame.shape[1]
+        """ Scan through all the bounding boxes output from the network and keep only the ones with high confidence scores. Assign the box's class label as 
+        the class with the highest score """
+        classIds = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                if confidence > self.conf_thres:
+                    center_x = int(detection[0] * frameWidth)
+                    center_y = int(detection[1] * frameHeight)
+                    width = int(detection[2] * frameWidth)
+                    height = int(detection[3] * frameHeight)
+                    left = int(center_x - width / 2)
+                    top = int(center_y - height / 2)
+                    classIds.append(classId)
+                    confidences.append(float(confidence))
+                    boxes.append([left, top, width, height])
+        """ Perform non maximum suppression to eliminate redundant overlapping boxes with lower confidences """
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, self.conf_thres, self.nms_thres)
+        bbox_container = []
+        for i in indices:
+            i = i[0]
+            [left, top, width, height] = boxes[i]
+            self.draw_image(frame, classIds[i], confidences[i], left, top, left + width, top + height)
+            bbox = {'class': self.classes[classIds[i]], 'confidence': round(confidences[i], 2), 'box': [left, top, left + width, top + height]}
+            bbox_container.append(bbox)
+        return bbox_container
+
+    def __call__(self, image, *args, **kwargs):
+        """ main function """
+        frame = image.copy()
+        """ Create a 4D blob from an image """
+        blob = cv2.dnn.blobFromImage(frame, 1/255, (self.imgsz, self.imgsz), [0, 0, 0], 1, crop=False)
+        """ Sets the input to the network """
+        self.net.setInput(blob)
+        """ Runs the forward pass to get output of the output layers """
+        output_layers = self.get_output_names()
+        outs = self.net.forward(output_layers)
+        """ Remove the bounding boxes with low confidence """
+        bbox_container = self.non_max_suppression(frame, outs)
+        """ Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in 
+        layersTimes) """
+        t, _ = self.net.getPerfProfile()
+        label = f'Inference time: {t * 1000.0 / cv2.getTickFrequency():.2f} ms'
+        cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        return frame, bbox_container
 
 
-def get_colors(names):
-    return {name: (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for name in names}
-
-
-# Get the names of the output layers
-def get_output_names(net):
-    # Get the names of all the layers in the network
-    layers_names = net.getLayerNames()
-    # Get the names of the output layers, i.e. the layers with unconnected outputs
-    return [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-
-def draw_image(image, predictions, classes, colors):
-    for x, y, w, h, prob, obj_id in predictions:
-        label = classes[obj_id]
-        # print(f'{obj_id:2d} {label.rjust(10)} {prob:.2f} {x:3d} {y:3d} {w:3d} {h:3d}')
-        # Draw a bounding box.
-        # (255, 178, 50)
-        cv.rectangle(image, (x, y), (x + w, y + h), colors[label], 2)
-        # Display the label at the top of the bounding box
-        label = f'{label}: {prob:.2f}'
-        label_size, base_line = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 2)
-        # cv.rectangle(image, (x+1, y+1), (x+label_size[0], y+label_size[1]+base_line), COLOR_W, cv.FILLED)
-        # cv.putText(image, label, (x+1, y+label_size[1]), FONT_SIMPLEX, 0.5, (0, 0, 0), 1, LINE_AA)
-        cv.putText(image, label, (x, y + label_size[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2, cv.LINE_AA)
-    return frame
-
-
-# Remove the bounding boxes with low confidence using non-maximum suppression
-def detect_image(frame, net, input_width, input_height, confidence_threshold, nms_threshold):
-    frame_height = frame.shape[0]
-    frame_width = frame.shape[1]
-    blob = cv.dnn.blobFromImage(frame, 1 / 255, (input_width, input_height), [0, 0, 0], 1, crop=False)
-    net.setInput(blob)
-    out_layers = net.forward(get_output_names(net))
-    # Candidates
-    class_ids = []
-    confidences = []
-    boxes = []
-    for out in out_layers:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > confidence_threshold:
-                center_x = int(detection[0] * frame_width)
-                center_y = int(detection[1] * frame_height)
-                width = int(detection[2] * frame_width)
-                height = int(detection[3] * frame_height)
-                left = int(center_x - width / 2)
-                top = int(center_y - height / 2)
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([left, top, width, height])
-    # Perform non maximum suppression
-    indices = cv.dnn.NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold)
-    predictions = []
-    for idx in indices:
-        i = idx[0]
-        box = boxes[i]
-        predictions.append([box[0], box[1], box[2], box[3], confidences[i], class_ids[i]])
-    return predictions
-
-
-if __name__ == '__main__':
-    # Initialization
-    confidence_threshold = 0.5
-    nms_threshold = 0.4
-    input_width = 320
-    input_height = 320
-    device = 'gpu'
-    names_path = "config/helmet.names"
-    cfg_path = "config/helmet.cfg"
-    weights_path = "weights/helmet.weights"
-    video_path = 'videos/helmet.mp4'
-    video_save_path = "../videos/result.avi"
-    # Load names
-    classes = get_classes(names_path)
-    colors = get_colors(classes)
-    # Create net
-    net = cv.dnn.readNetFromDarknet(cfg_path, weights_path)
-    # cpu or gpu
-    if device == 'gpu':
-        net.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-        net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
-        print('Using GPU device.')
+def detect_demo():
+    image_name = 'dog.jpg'
+    image_path = f'images/{image_name}'
+    image_save_path = f'images/_{image_name}'
+    video_name = 'run.mp4'
+    video_path = f'videos/{video_name}'
+    video_save_path = f'videos/_{video_name}'
+    # image_path = ''
+    if image_path:
+        cap = cv2.VideoCapture(image_path)
     else:
-        net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-        net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-        print('Using CPU device.')
-    # Process inputs
-    winName = 'Deep learning object detection in OpenCV'
-    cv.namedWindow(winName, cv.WINDOW_NORMAL)
-    # Open the video
-    cap = cv.VideoCapture(video_path)
-    image_width = round(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-    image_height = round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-    cv.resizeWindow(winName, image_width, image_height)
-    cv.moveWindow(winName, 50, 50)
-    # Initialize the video writer
-    # vid_writer = cv.VideoWriter(video_save_path, cv.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (image_width, image_height))
-    while cv.waitKey(1) < 0:
+        cap = cv2.VideoCapture(video_path)
+        fource = cv2.VideoWriter_fourcc(*'mp4v')
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        vid_writer = cv2.VideoWriter(video_save_path, fource, 30, (width, height))
+    """ 图像检测器 """
+    yolov4_detector = YOLOv4DetectorCV()
+    window_name = 'YOLOv4 detector'
+    while True:
         state, frame = cap.read()
         if not state:
             break
-        # Detect
-        predictions = detect_image(frame, net, input_width, input_height, confidence_threshold, nms_threshold)
-        # Draw
-        frame = draw_image(frame, predictions, classes, colors)
-        t, _ = net.getPerfProfile()
-        label = f'Inference time: {t * 1000.0 / cv.getTickFrequency():.2f} ms'
-        cv.putText(frame, label, (0, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
-        # Save
-        # vid_writer.write(frame.astype(np.uint8))
-        cv.imshow(winName, frame)
+        image_result, bbox_container = yolov4_detector(frame)
+        for info in bbox_container:
+            print(info)
+        print('---')
+        """ 显示图像 """
+        cv2.imshow(window_name, image_result)
+        if image_path:
+            cv2.imwrite(image_save_path, image_result)
+            cv2.waitKey(3000)
+        else:
+            vid_writer.write(image_result)
+            cv2.waitKey(1)
+        """ 点 x 退出 """
+        if cv2.getWindowProperty(window_name, cv2.WND_PROP_AUTOSIZE) < 1:
+            break
     cap.release()
+    if not image_path:
+        vid_writer.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    detect_demo()
