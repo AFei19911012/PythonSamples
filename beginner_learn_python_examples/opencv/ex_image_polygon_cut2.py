534d01c9772bdb364c06148a58d069fc97342773
ff_wang <1105936347@qq.com>
2021-09-12 23:48:53 +0800

    :see_no_evil:

---
+++ beginner_learn_python_examples/opencv/ex_image_polygon_cut2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+ Created on 2021/9/12 21:22
+ Filename   : ex_image_polygon_cut2.py
+ Author     : Taosy.W
+ Zhihu      : https://www.zhihu.com/people/1105936347
+ Github     : https://github.com/AFei19911012/PythonSamples
+ Description:
+"""
+
+# =======================================================
